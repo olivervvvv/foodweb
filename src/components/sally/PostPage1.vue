40a8a30b99c6b9b7aeed4069ac524ac17f2f4337
zxc1579820gn <lin123963123@gmail.com>
2024-01-03 14:10:35 +0800

    postpage更新

--- src/components/sally/PostPage1.vue
+++ src/components/sally/PostPage1.vue
@@ -304,7 +304,7 @@ export default {
                 </div>
                 <div class="score">
                     <i class="fa-regular fa-star"></i>
-                    <span>{{ storeInfo.userLike.toFixed(2) }}</span>
+                    <span>{{ storeInfo.userLike}}</span>
                 </div>
             </div>
         </div>
